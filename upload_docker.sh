bc5690ee0ef4283b9a52ae2292470c447bb2ccd1
Mai Thien An <maian1891@gmail.com>
2023-07-26 21:55:13 +0700

    Upload the Docker Image

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,18 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+# $DOCKER_USERNAME will be export to system environment,
+# like if we use circleci, we can create DOCKER_USERNAME variable
+dockerpath=$DOCKER_USERNAME/machine-learning-microservice
 
-# Step 2:  
+# Step 2:
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+echo "Login docker..."
+# $DOCKER_PASSWORD will be export to system environment,
+# like if we use circleci, we can create DOCKER_PASSWORD variable
+echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAM --password-stdin docker.io
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
