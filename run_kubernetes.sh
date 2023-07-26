97e3973ac422ef4540e58b24a1e087d509e1b14b
Mai Thien An <maian1891@gmail.com>
2023-07-26 22:20:49 +0700

    Configure Kubernetes to Run Locally

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,17 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=$DOCKER_USERNAME/machine-learning-microservice
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run machine-learning-microservice --image $dockerpath
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward machine-learning-microservice 8000:80
 
